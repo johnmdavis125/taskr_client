0500810f2e64cc9208ca62bb4de076fbd563d687
John Davis <johnmdavis125@gmail.com>
2021-02-14 21:50:37 -0500

    working on handleChange and handleSubmit stopping for the day

--- src/App.js
+++ src/App.js
@@ -24,22 +24,60 @@ export default function App() {
     })();
   }, []);
 
+  const handleChange = (e) => {
+    updateNewTask({
+      ...newTask.title,
+      [e.target.id]: e.target.value
+    })
+    console.log(newTask)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    (async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'},
+          body: JSON.stringify(newTask.title)
+        });
+      const data = response.json();
+      console.log(data)
+      await updateAllTasks([...allTasks, data]);
+      await updateNewTask({
+        title: '',
+        completed: false
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+}
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='newTask'>Enter a new Task</label>
-        <input type='text' id='newTask' name='newTask'></input>
+        <input type='text' id='newTask' name='newTask' value={newTask.title} placeholder='Enter a task here' onChange={handleChange}></input>
       </div>
+      <input 
+          type='submit'
+          name=''
+          value='Create Task'
+          ></input>
     </form>
 
     
     {allTasks.length > 0 &&
     allTasks.map((newTask) => {
       return (
-        <h4>{newTask.title}</h4>
+        <div>
+        <h4>Task: {newTask.title}</h4>
+        <h6>Completed: {newTask.completed === true ? 'true' : 'false'}</h6>
+        </div>
       )
     })
   }
